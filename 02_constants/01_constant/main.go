3438263fac514c549e3d53c3bff3efe04aac8426
Robert <robertlowry8597@gmail.com>
2017-06-28 12:38:19 -0400

    adds 02-05

---
+++ 02_constants/01_constant/main.go
@@ -0,0 +1,15 @@
+package _1_constant
+
+import "fmt"
+
+const p string = "death & taxes"
+
+
+
+func main() {
+
+	const q = 42
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+}
---
+++ 05_remainder/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	z := 13 / 3
+	fmt.Println(z)
+	x := 13 % 3
+	fmt.Println("R=", x)
+}
